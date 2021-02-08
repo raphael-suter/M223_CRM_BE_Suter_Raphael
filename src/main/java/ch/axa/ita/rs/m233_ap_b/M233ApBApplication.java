ab32ae118b5854f6694666d157849f9d1b228f9f
Raphael Suter <raphael.werner.suter@gmail.com>
2021-02-08 13:54:16 +0100

    Set up project

---
+++ src/main/java/ch/axa/ita/rs/m233_ap_b/M233ApBApplication.java
@@ -0,0 +1,13 @@
+package ch.axa.ita.rs.m233_ap_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M233ApBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M233ApBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/axa/ita/rs/m233_ap_b/M233ApBApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.axa.ita.rs.m233_ap_b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M233ApBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
